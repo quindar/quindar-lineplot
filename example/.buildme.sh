1afae27fa5d365dc0986d222947c8791dfe772e5
Masaki <masaki.kakoi@audacy.space>
2016-06-28 16:34:10 -0700

    angular line plot stand alone

---
+++ example/.buildme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+// Program: buildme.sh
+// Purpose: to install NodeJS depencencies
+// Author:  Masaki Kakoi
+// Updated: Jun 28, 2016
+//
+echo "Building NodeJS dependencies for Quindar line plot"
+echo "...installing npm modules from package.json"
+npm install
+echo "...complete"
